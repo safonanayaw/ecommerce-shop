f33302172554ee4f9e7e47274c386759b1e23c76
safonanayaw <safoasare17@gmail.com>
2024-08-24 23:46:59 +0100

    best seller component complete

---
+++ src/components/OurPolicy.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const OurPolicy = () => {
+  return (
+    <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700'>
+      <div>
+        <img src={assets.exchange_icon} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default OurPolicy
