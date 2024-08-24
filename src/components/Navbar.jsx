5101f943e306b7ff5acbd1ef707799edce2eed88
safonanayaw <safoasare17@gmail.com>
2024-08-24 20:56:12 +0100

    latext collection component create

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,10 @@ const Navbar = () => {
 
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
+      <Link to='/'>
       <img src={assets.logo} className='w-36' alt="" />
+      </Link>  
+      
       <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
         <NavLink to='/' className='flex flex-col items-center gap-1'>
             <p>Home</p>
@@ -58,7 +61,7 @@ const Navbar = () => {
                 <p>Back</p>
             </div>
             {/* onclick logic for display exist the dropdown list when any is click */}
-            <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/'>Home</NavLink>
+            <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/'>HOME</NavLink>
             <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/collection'>COLLECTION</NavLink>
             <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/about'>ABOUT</NavLink>
             <NavLink onClick={()=> setVisible(false)} className='py-2 pl-6 border' to='/contact'>CONTACT</NavLink>
