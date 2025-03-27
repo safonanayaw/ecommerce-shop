dba78ebac312c86feb3b9326ce26745072aa83da
safonanayaw <safoasare17@gmail.com>
2025-03-27 00:41:37 +0000

    frontend App.jsx update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,7 +11,7 @@ import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import SearchBar from './components/SearchBar'
+import SearchBar from './components/searchBar'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Verify from './pages/Verify'
